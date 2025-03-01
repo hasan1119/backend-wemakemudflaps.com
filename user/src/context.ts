df3916ac853ca1113c81b224ab09e0bd032850f1
JoySarkarBD <developer.joysarkar@gmail.com>
2025-03-01 17:44:58 +0600

    chore: unwanted code removed form the context.ts

--- user/src/context.ts
+++ user/src/context.ts
@@ -2,11 +2,9 @@
 
 import { IncomingMessage, ServerResponse } from "http";
 import { DataSource } from "typeorm";
-import { UserSession } from "./middleware/context";
 
 export type Context = {
   AppDataSource: DataSource;
-  user: UserSession;
   ip: string;
   acceptLanguage: string;
   languages: [];
