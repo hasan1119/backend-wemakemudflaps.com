849662e20b328001cbf876307de37f560c08cb84
hasan1119 <rokibul.devs@gmail.com>
2025-07-29 12:24:44 +0600

    feat: add logging plugin with pino for GraphQL requests and responses

--- product/src/plugins/loggingPlugin.ts
+++ product/src/plugins/loggingPlugin.ts
@@ -1,5 +1,5 @@
 import { ApolloServerPlugin } from "@apollo/server";
-import logger from "../../../users/src/utils/logger";
+import logger from "../utils/logger";
 
 const loggingPlugin: ApolloServerPlugin = {
   async requestDidStart(requestContext) {
