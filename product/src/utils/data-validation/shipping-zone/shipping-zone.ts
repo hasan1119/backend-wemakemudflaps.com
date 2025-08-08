dedc1ffa3c0c50f0530807dd031a12265f518139
JoySarkarBD <developer.joysarkar@gmail.com>
2025-08-08 17:40:20 +0600

    chore: shipping zone updated

--- product/src/utils/data-validation/shipping-zone/shipping-zone.ts
+++ product/src/utils/data-validation/shipping-zone/shipping-zone.ts
@@ -6,8 +6,8 @@ import { SortOrderTypeEnum } from "../common/common";
  *
  * Workflow:
  * 1. Validates name as a non-empty string with a minimum length of 2 characters.
- * 2. Validates regions as an array of non-empty strings with a minimum length of 2 characters.
- * 3. Validates zipCodes as an optional array of strings with a minimum length of 2 characters.
+ * 2. Validates regions as an array of non-empty strings with a minimum length of 1 characters.
+ * 3. Validates zipCodes as an optional array of strings with a minimum length of 1 characters.
  *
  * @property name - Shipping zone name (required).
  * @property regions - Array of regions (required).
@@ -15,8 +15,8 @@ import { SortOrderTypeEnum } from "../common/common";
  */
 export const createShippingZoneSchema = z.object({
   name: z.string().min(2).max(100),
-  regions: z.array(z.string().min(2).max(100)),
-  zipCodes: z.array(z.string().min(2).max(20)).optional(),
+  regions: z.array(z.string().min(1).max(100)),
+  zipCodes: z.array(z.string().min(1).max(20)).optional(),
 });
 
 /**
@@ -24,8 +24,8 @@ export const createShippingZoneSchema = z.object({
  *
  * Workflow:
  * 1. Validates id as a UUID string.
- * 2. Validates name as an optional string with a minimum length of 2 characters.
- * 3. Validates regions as an optional array of non-empty strings with a minimum length of 2 characters.
+ * 2. Validates name as an optional string with a minimum length of 1 characters.
+ * 3. Validates regions as an optional array of non-empty strings with a minimum length of 1 characters.
  * 4. Validates shippingMethodIds as an optional array of UUID strings.
  *
  * @property id - Shipping zone ID (required).
@@ -37,8 +37,8 @@ export const updateShippingZoneSchema = z
   .object({
     id: z.string().uuid(),
     name: z.string().min(2).max(100).optional().nullable(),
-    regions: z.array(z.string().min(2).max(100)).optional().nullable(),
-    zipCodes: z.array(z.string().min(2).max(20)).optional().nullable(),
+    regions: z.array(z.string().min(1).max(100)).optional().nullable(),
+    zipCodes: z.array(z.string().min(1).max(20)).optional().nullable(),
   })
   .refine(
     (data) =>
